cb15dd70af8a0d27418ee5bdf5d6a8cfb246b0c6
Mark Standen <markstanden80@icloud.com>
2020-05-18 23:26:51 +0100

    managed to get requests from last.fm

---
+++ README.md
@@ -0,0 +1,14 @@
+# required command line ninjitsu
+
+Written in python3, so make sure the python3 interpreter is installed
+
+then make the last.fm api available
+
+pip install pylast 
+or pip3 install pylast
+
+
+To play some music make sure the music subdirectory has music in it, then launch using
+
+python last_cli_player.py
+or python3 last_cli_player.py
--- last-cli-player.py
+++ last-cli-player.py
@@ -3,19 +3,14 @@
     For simplicity the songs provided are used
 """
 
-# required command line ninjitsu
-# pip3 install pylast
-
-# the last.fm api
-import pylast
-
-# for file file handling
-import io
-
-# for the filename to call
-#from sys import argv
-
+import eyed3
 import playsound as ps
+import io
+import os
+import requests
+import json
+
+API_KEY = "9508bed5c4e8d5e91c63fc1d91c4702e"
 
 def play_file (path_and_file):
     """ Plays the file sent the function"""
@@ -27,31 +22,86 @@ def get_tags(path_and_file):
     """ Gets tags from the file, this is to identify
         the song to last.fm for scrobbling"""
 
-    tags = {'Artist':'Unknown',
-            'Album':'Unknown',
-            'Track':'Unknown'
+    musicfile = eyed3.load(path_and_file)
+
+    tags = {'Title':musicfile.tag.title,
+            'Artist':musicfile.tag.artist,
+            'Album':musicfile.tag.album,
+            'Track':musicfile.tag.track_num[0]
             }
-    # tag lookup goes here     
+
     return tags
 
 
-def get_lfm(artist, album, track)
+def get_lfm():
     """ Looks up relevant information about track to be played
-        returns a list of stuff """
+    #     returns a list of stuff """
 
-    last_data = [artist, album, track]
-    return last_data
+    
 
-
-    tags = get_tags(path_and_file)
-    print(tags)
+    # last_data = [artist, album, track]
+    # return last_data
 
 
-music_path = 'Music/' 
-file1 = 'Gillicuddy_-_01_-_Adventure_Darling.mp3'
-file2 = 'Kevin_MacLeod_-_08_-_Slow_Burn.mp3'
+    # tags = get_tags(path_and_file)
+    # print(tags)
 
-#ps.playsound('Music/Gillicuddy_-_01_-_Adventure_Darling.mp3')
-#ps.playsound('Music/Kevin_MacLeod_-_08_-_Slow_Burn.mp3')
+def last_get_trackinfo(tag_dict):
+    """ From last.fm api examples:
+        http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=YOUR_API_KEY&artist=cher&track=believe&format=json
+        The API requires the request to be sent in this format.  Source (https://www.last.fm/api/show/album.getInfo)
+    """
 
-play_file(music_path + file1)
+    last_url = 'http://ws.audioscrobbler.com/2.0/?'
+
+    params = {
+              'method':'track.getInfo',
+              'api_key':API_KEY,
+              'artist':tag_dict['Artist'],
+              'track':tag_dict['Title'],
+              'format':'json',
+    }
+    
+    #requests.get(url, params={key: value}, args) 
+    return requests.get(last_url, params=params)
+
+def last_get_similar(tag_dict):
+    """ From last.fm api examples:
+        http://ws.audioscrobbler.com/2.0/?method=track.getsimilar&artist=cher&track=believe&api_key=YOUR_API_KEY&format=json
+        The API requires the request to be sent in this format.  Source (https://www.last.fm/api/show/track.getSimilar)
+    """
+
+    last_url = 'http://ws.audioscrobbler.com/2.0/?'
+
+    params = {
+              'method':'track.getSimilar',
+              'api_key':API_KEY,
+              'artist':tag_dict['Artist'],
+              'album':tag_dict['Album'],
+              'format':'json',
+    }
+    
+    #requests.get(url, params={key: value}, args) 
+    return requests.get(last_url, params=params)
+
+
+import json
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=1)
+    print(text)
+    
+music_path = 'Music/'
+music_files = os.listdir(music_path)
+# print(arr)
+
+# file1 = 'Gillicuddy_-_01_-_Adventure_Darling.mp3'
+# file2 = 'Kevin_MacLeod_-_08_-_Slow_Burn.mp3'
+id3_tags = get_tags(music_path + music_files[0])
+
+track_info = last_get_trackinfo(id3_tags)
+similar_info = last_get_similar(id3_tags)
+jprint(track_info.json())
+jprint(similar_info.json())
+play_file(music_path + music_files.pop(0))
